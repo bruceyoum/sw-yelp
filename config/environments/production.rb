0c0a6e1766ff959f4d4d4351e8ff28fa69d3d659
“bruceyoum” <bruceyoum@gmail.com>
2017-07-19 15:13:53 -0400

    Add user accounts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for devise gem. Make sure to change to domain name xyz.com later.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 end
